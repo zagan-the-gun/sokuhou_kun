a87ef372d4191469ee38808d50b6c7cc9d124318
zagan-the-gun <zagan.the.gun@gmail.com>
2024-01-09 02:55:15 +0900

    モデル構成ザクっと変更

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,10 +1,10 @@
-from typing import List
-
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+
+from typing import List
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -19,63 +19,22 @@ def get_db():
     finally:
         db.close()
 
-
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
-
-
-@app.get("/games/", response_model=List[schemas.Game])
-def read_games(skip: int = 0, limit: int = 100, delivery: bool = False, db: Session = Depends(get_db)):
-    games = crud.get_games(db, skip=skip, limit=limit, delivery=delivery)
-    return games
-
-
-@app.put('/games/{id}', response_model=schemas.Game)
-def update_game(
-    id: int,
-    #request: GameCreate,
-    game: schemas.GameUpdate,
-    db: Session = Depends(get_db)
-):
-    #return db_article.update_article(db, id, request)
-    return crud.update_game(db=db, game=game,id=id)
-
-
-@app.post("/games/", response_model=schemas.Game)
+@app.post("/games/", response_model=schemas.GameCreateSchema)
 def create_game(
-    game: schemas.GameCreate, db: Session = Depends(get_db)
+    game: schemas.GameCreateSchema, db: Session = Depends(get_db)
 ):
     return crud.create_game(db=db, game=game)
 
+@app.get("/games/", response_model=List[schemas.GameCreateSchema])
+def read_games(skip: int = 0, limit: int = 100, is_sent: bool = False, db: Session = Depends(get_db)):
+    games = crud.get_games(db, skip=skip, limit=limit, is_sent=is_sent)
+    return games
+
+@app.put('/games/{id}', response_model=schemas.GameUpdateSchema)
+def update_game(
+    id: int,
+    game: schemas.GameUpdateSchema,
+    db: Session = Depends(get_db)
+):
+    return crud.update_game(db=db, game=game,id=id)
 
