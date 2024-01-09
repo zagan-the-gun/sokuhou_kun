c1c38fa79d011a0b5210dc62e185e48e131b626b
zagan-the-gun <zagan.the.gun@gmail.com>
2024-01-09 18:21:23 +0900

    パスワードを秘匿対応

--- discord.py
+++ discord.py
@@ -1,12 +1,23 @@
-#!/usr/bin/python3.7
+#!/usr/bin/python3.10
 # -*- coding: utf8 -*-
 
 import requests
 import json
+import os
+import sys
 
 
+# TOKENチェック
+if len(sys.argv) >= 2:
+    DISCORD_TOKEN = sys.argv[1]
+elif os.path.isfile('./DISCORD_TOKEN'):
+    with open('./DISCORD_TOKEN', 'r') as f:
+        DISCORD_TOKEN = f.read().splitlines()[0]
+else:
+    print('DISCORD_TOKEN not found')
+    sys.exit(1)
 
-target_url = "https://dev-common.maskapp.club/games/?skip=0&limit=100&delivery=false"
+target_url = "http://192.168.1.5/games/?skip=0&limit=100&is_sent=false"
 response = requests.get(target_url)
 game = json.loads(response.text)
 for g in game:
@@ -16,16 +27,17 @@ for g in game:
     headers = {'content-type': 'application/json'}
     data = json.dumps({
         'username'   : '速報くん',
-        'content'    : '**' + g['name'] + '** : ' + g['url'],
+        #'content'    : '**' + g['name'] + '** : ' + g['url'],
+        'content'    : '**' + g['url'] + '**',
         })
-    result = requests.post("https://discordapp.com/api/webhooks/566122139501985817/Gi0obOGCSx14aEiMGyCXo_7vFW30KI7BuLbEcr9UAx19Re0hqV_y5bF2x4-XzTE_YQCO", data, headers=headers)
+    result = requests.post('https://discord.com/api/webhooks/' + DISCORD_TOKEN, data, headers=headers)
     print(result)
 
-    # 送信済みフラグ
+    # 送信済みフラグを立てる
     headers = {'content-type': 'application/json'}
     data = json.dumps({
-        'delivery': True,
+        'is_sent': True,
         })
-    result = requests.put("https://dev-common.maskapp.club/games/" + str(g['id']), data, headers=headers)
+    result = requests.put("http://192.168.1.5/games/" + str(g['id']), data, headers=headers)
     print(result)
 
