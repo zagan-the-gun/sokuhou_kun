c1167f9f26e846a3571e000f404a21402465ca64
zagan-the-gun <zagan.the.gun@gmail.com>
2024-01-09 17:06:23 +0900

    updateを機能するように修正

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -16,20 +16,12 @@ def create_game(db: Session, game: schemas.GameCreateSchema):
     return db_game
 
 def get_games(db: Session, skip: int = 0, limit: int = 100, is_sent: bool = False):
-#    return db.query(models.Game).filter_by(is_sent=is_sent).offset(skip).limit(limit).all()
-    return db.query(models.Game).all()
+    return db.query(models.Game).filter_by(is_sent=is_sent).offset(skip).limit(limit).all()
+#    return db.query(models.Game).all()
 
 def update_game(db: Session, id: int, game: schemas.GameUpdateSchema):
     db_game = db.query(models.Game).filter_by(id=id).first()
-    """
-    db_game.update({
-        delivery: game.delivery,
-    })
-    db_game.commit()
-    return db_game
-    """
-
-#    db_game.is_sent = game.is_sent
+    db_game.is_sent = game.is_sent
 
     db.commit()
     db.refresh(db_game)
